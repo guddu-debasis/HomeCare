21a00292c6a20d3da997b7354e5318ce9c6af9c3
guddu-debasis <167549811+guddu-debasis@users.noreply.github.com>
2026-09-05 15:43:24 +0530

    feat:add dtos in admin and seller

---
+++ src/modules/admin/dto/login.dto.js
@@ -0,0 +1,13 @@
+import joi from "joi";
+import BaseDto from "../../../common/dto/base.dto.js"
+
+class LoginDto extends BaseDto {
+    static schema = joi.object({
+        email: joi.string().email().lowercase().required(),
+        password: joi.string()
+        .message("Password must contain 8 chars minimum")
+        .min(8).required()
+    })
+}
+
+export default LoginDto
---
+++ src/modules/seller/dto/register.dto.js
@@ -0,0 +1,16 @@
+import joi from "joi";
+import BaseDto from "../../../common/dto/base.dto.js"
+
+class RegisterDto extends BaseDto {
+    static schema = joi.object({
+        name: joi.string().alphanum().min(3).max(30).required(),
+        email: joi.string().email().lowercase().required(),
+        password: joi.string()
+        .message("Password must contain 8 chars minimum")
+        .min(8).required(),
+        role: joi.string().valid(["babycare","housekeeping","cooking","eldercare","tutoring"]).required(),
+        dateOfBirth: joi.date().iso().required()
+    })
+}
+
+export default RegisterDto
