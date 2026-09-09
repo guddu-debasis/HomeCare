e12887b351b73dd081ab84461a93dae357568bd0
guddu-debasis <167549811+guddu-debasis@users.noreply.github.com>
2026-09-09 18:34:24 +0530

    feat:correect compare password

--- src/modules/customer/customer.service.js
+++ src/modules/customer/customer.service.js
@@ -52,7 +52,7 @@ const login = async ({ email, password }) => {
     throw ApiError.forbidden("Please verify your email before loggin");
   }
 
-  const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await user.comparePassword(password);
   if (!isMatch) throw ApiError.unauthorized("Invalid Email or password");
 
   const accessToken = generateAccessToken({ id: user._id, role: user.role });
